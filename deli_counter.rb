517030f004e12f4a56d2409534018b0fb8772fac
Danny Padron <danny.padron09@gmail.com>
2020-04-15 22:41:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,14 @@ def take_a_number(line, new_person)
   line << new_person
   puts "Welcome, #{new_person}. You are number #{line.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    "There are no people in line."
+  end
+end
+
+
+
+
+    
