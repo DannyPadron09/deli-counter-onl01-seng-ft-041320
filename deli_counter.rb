76b02a3d4e072f70b0f14de379f78cd2d7806cc7
Danny Padron <danny.padron09@gmail.com>
2020-04-15 21:45:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,11 @@
 
 
 def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty"
-  elsif katz_deli.length >= 1
+  if katz_deli.length >= 1
     current_line = []
     counter = 1
     katz_deli.each do |name|
-      current_line << "The current line is #{counter}. #{name}"
+      current_line << "#{counter}.. #{name}"
       counter += 1
     end
   end
