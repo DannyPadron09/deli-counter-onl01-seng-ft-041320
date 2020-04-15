c60a88d3b8034844de8602f14ca504853db8f6a0
Danny Padron <danny.padron09@gmail.com>
2020-04-15 21:35:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,6 @@
 
 def line(katz_deli)
   if katz_deli.length == 0
-    "The line is currently empty"
-  elsif katz_deli.length > 0
-    "You are #{katz_deli.length} in line"
+    puts "The line is currently empty"
   end
 end
